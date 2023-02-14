20ae2820785ed56bbaa580695f50fc921c66d58c
itismhn <itismhn@yahoo.com>
2023-02-15 03:07:23 +0330

    insert condition for input option

--- port-scanner.py
+++ port-scanner.py
@@ -2,9 +2,14 @@ def input_validation():
     print("[~]Enter 1 for scan [1-1024] ports\n[~]Enter 2 for scan [1-65535] ports\n[~]Enter 3 for scan custom ports")
     while True:
         try:   
-            option =int(input("[~]Enter Number:"))
-            if option in [1,2,3]:
-                print('ok')
+            input_option =int(input("[~]Enter Number:"))
+            if input_option in [1,2,3]:
+                if input_option==1:
+                    print(1)
+                elif input_option==2:
+                    print(2)
+                elif input_option==3:
+                    print(3)
             else:
                 print('[~]please choose valid option\n')
         except KeyboardInterrupt:
@@ -12,18 +17,6 @@ def input_validation():
         except :
             print('[~]oops! Enter a valid Charecters!\n')
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
 input_validation()
