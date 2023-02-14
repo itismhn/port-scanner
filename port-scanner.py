eda35810f13d59d2e75ce4baa88abf687b4bf7bc
itismhn <itismhn@yahoo.com>
2023-02-15 03:17:07 +0330

    import os

--- port-scanner.py
+++ port-scanner.py
@@ -1,3 +1,6 @@
+import os
+
+os.system('cls')
 def input_validation():
     print("[~]Enter 1 for scan [1-1024] ports\n[~]Enter 2 for scan [1-65535] ports\n[~]Enter 3 for scan custom ports")
     while True:
