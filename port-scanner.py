ad9198a32a4dc7e0d491e509e769415bb434862c
itismhn <itismhn@yahoo.com>
2023-02-15 02:48:06 +0330

    add event handling in inputs

--- port-scanner.py
+++ port-scanner.py
@@ -1,9 +1,17 @@
 def input_validation():
     print("[~]Enter 1 for scan [1-1024] ports\n[~]Enter 2 for scan [1-65535] ports\n[~]Enter 3 for scan custom ports")
-    option =int(input("Enter Number:"))
-    if option in [1,2,3]:
-        print('ok')
-    else:
-        print('please choose valid option')
+    while True:
+        try:   
+            option =int(input("[~]Enter Number:"))
+            if option in [1,2,3]:
+                print('ok')
+            else:
+                print('[~]please choose valid option\n')
+        except KeyboardInterrupt:
+            break
+        except :
+            print('[~]oops! Enter a valid Charecters!\n')
+        
+
 
 input_validation()
