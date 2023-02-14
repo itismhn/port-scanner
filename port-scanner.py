c552480c83cd728bd4549eaed748a6f6573c9afe
itismhn <itismhn@yahoo.com>
2023-02-15 02:36:46 +0330

    input validation

--- port-scanner.py
+++ port-scanner.py
@@ -1,1 +1,9 @@
-print('hello world!)
+def input_validation():
+    print("[~]Enter 1 for scan [1-1024] ports\n[~]Enter 2 for scan [1-65535] ports\n[~]Enter 3 for scan custom ports")
+    option =int(input("Enter Number:"))
+    if option in [1,2,3]:
+        print('ok')
+    else:
+        print('please choose valid option')
+
+input_validation()
